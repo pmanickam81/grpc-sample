ebf990aec91579a7eeb0838dab4ac36f7ad4e7d4
Manickam Paramasivan <manickam.paramasivan@hidglobal.com>
2020-04-14 19:36:01 +0530

    grpc initial commit

---
+++ src/main/java/org/example/grpc/client/GreetingClient.java
@@ -0,0 +1,23 @@
+package org.example.grpc.client;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.example.greet.GreetRequest;
+import org.example.greet.GreetResponse;
+import org.example.greet.GreetServiceGrpc;
+import org.example.greet.Greeting;
+
+public class GreetingClient {
+
+    public static void main(String[] args) {
+        System.out.println("Hello gRPC Client");
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext().build();
+        GreetServiceGrpc.GreetServiceBlockingStub greetClient = GreetServiceGrpc.newBlockingStub(channel);
+        Greeting greeting = Greeting.newBuilder().setFirstName("John").setLastName("Smith").build();
+        GreetRequest greetRequest = GreetRequest.newBuilder().setGreeting(greeting).build();
+        GreetResponse greetResponse = greetClient.greet(greetRequest);
+        System.out.println("The Response is : " + greetResponse.getResult());
+        channel.shutdown();
+    }
+
+}
---
+++ src/main/java/org/example/grpc/server/GreetServiceImpl.java
@@ -0,0 +1,19 @@
+package org.example.grpc.server;
+
+import io.grpc.stub.StreamObserver;
+import org.example.greet.*;
+
+public class GreetServiceImpl extends GreetServiceGrpc.GreetServiceImplBase {
+
+    @Override
+    public void greet(GreetRequest request, StreamObserver<GreetResponse> responseObserver) {
+        System.out.println("Inside greet method in GreetServiceImpl");
+        Greeting greeting = request.getGreeting();
+        String firstName = greeting.getFirstName();
+        String lastName = greeting.getLastName();
+        String result = "Hello " + firstName +" " + lastName + "!" ;
+        GreetResponse response = GreetResponse.newBuilder().setResult(result).build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
